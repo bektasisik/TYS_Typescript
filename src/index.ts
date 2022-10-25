0b8a7c67c26b5feebed24564fca804a9d51786b1
Bektas Isik <bektas.isik18@gmail.com>
2022-10-25 18:45:04 +0300

    İndex.ts dosyasına student CRUD işlemi gerçekleştirildi.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,110 @@
-/*const name = document.getElementById("name").value
-const surname = document.getElementById("surname").value
-const button = document.getElementById("addButton")
-button.addEventListener("click", () => {
-  const fullname = name + " " + surname;
-alert("Talebe eklendi: " + fullname);
-});*/
-
 import { StudentService } from "./service/StudentService";
 import { AttendanceService } from "./service/AttendanceService";
+import { debug } from "webpack";
 
-const studentUi = document.getElementById("studentUi");
-const studentSection = document.getElementById("studentSection");
+const studentService = new StudentService();
+const attendanceService = new AttendanceService();
+
+addStudent();
+/* function addStudent() {
+  const name = document.getElementById("name") as HTMLInputElement;
+  const surname = document.getElementById("surname") as HTMLInputElement;
+  studentService.addStudent(name.value, surname.value);
+  addStudentList();
+} */
+
+function addStudent() {
+  debugger;
+  const name = document.getElementById("name") as HTMLInputElement;
+  const surname = document.getElementById("surname") as HTMLInputElement;
+  const addButton = document.getElementById("addButton") as HTMLButtonElement;
+  addButton.addEventListener("click", () => {
+    const fullname = name.value + " 123123 " + surname.value;
+    studentService.addStudent(name.value, surname.value);
+    debugger;
+    console.log("Talebe eklendi: " + fullname);
+    name.value = "";
+    surname.value = "";
+    debugger;
+    console.log(studentService.getStudents());
+    debugger;
+    addStudentList();
+  });
+} 
+function addStudentList() {
+  const tbodyList = document.getElementById("studentListBody") as HTMLTableSectionElement;
+  debugger;
+  for (let index = 0; index < studentService.getStudents.length; index++) {
+    debugger;
+    const tr = document.createElement("tr");
+    const tdId = document.createElement("td");
+    const tdName = document.createElement("td");
+    const tdSurname = document.createElement("td");
+    const tdAbsent = document.createElement("td");
+    const tdButton = document.createElement("td");
+    const deletebutton = document.createElement("button");
+    const updatebutton = document.createElement("button");
+
+    tdId.innerText = studentService.getStudents()[index].id.toString();
+    tdName.innerText = studentService.getStudents()[index].name;
+    tdSurname.innerText = studentService.getStudents()[index].surname;
+    tdAbsent.innerText = studentService.getStudents()[index].absent.toString();
+    deletebutton.innerText = "Sil";
+    updatebutton.innerText = "Güncelle";
+    deletebutton.addEventListener("click", () => {
+      studentService.deleteStudent(index);
+      console.log(studentService.getStudents());
+      addStudentList();
+    });
+    updatebutton.addEventListener("click", () => {
+      const name = document.getElementById("name") as HTMLInputElement;
+      const surname = document.getElementById("surname") as HTMLInputElement;
+      studentService.updateStudent(index, name.value, surname.value);
+      console.log(studentService.getStudents());
+      addStudentList();
+    });
+    
+    
+    tr.appendChild(tdId);
+    tr.appendChild(tdName);
+    tr.appendChild(tdSurname);
+    tr.appendChild(tdAbsent);
+    tdButton.appendChild(deletebutton);
+    tdButton.appendChild(updatebutton);
+    tr.appendChild(tdButton);
+    
+    console.log(tr)
+    tbodyList.appendChild(tr);
+  }
+}
+function showStudents() {
+  const studentList = document.getElementById("studentList") as HTMLUListElement;
+  studentList.innerHTML = "";
+  const students = studentService.getStudents();
+  students.forEach((student) => {
+    const li = document.createElement("li");
+    li.innerText = student.name + " " + student.surname;
+    studentList.appendChild(li);
+  });
+}
+function deleteStudent(id: number) {
+  studentService.deleteStudent(id);
+  addStudentList();
+}
+function updateStudent(id: number) {
+  const updateName = document.getElementById("name") as HTMLInputElement;
+  const updateSurname = document.getElementById("surname") as HTMLInputElement;
+  const updateButton = document.getElementById("addButton") as HTMLButtonElement;
+  updateButton.innerText = "Güncelle";
+  updateButton.addEventListener("click", () => {
+    studentService.updateStudent(id, updateName.value, updateSurname.value);
+    updateButton.innerText = "Ekle";
+    addStudentList();
+  });
+}
 
 
-let sss = new StudentService();
-let saaass = new AttendanceService();
+
+
+
 
--- src/service/StudentService.ts
+++ src/service/StudentService.ts
@@ -1,19 +1,19 @@
 import { Student } from "../domain/Student";
 
 export class StudentService {
-    students!: Array<Student>;
-    sequence: number = 1;
+    private _students: Array<Student> = new Array();
+    private _sequence: number = 1;
 
     addStudent(name: string, surname: string) {
-        let student = new Student(this.sequence++, name, surname)
-        this.students.push(student);
+        const student = new Student(this._sequence++, name, surname)
+        this._students.push(student);
     }
 
     getStudents(): Array<Student> {
-        return this.students;
+        return this._students;
     }
     getStudent(studentId: number): Student {
-        return this.students.filter(student => student.id === studentId)[0];
+        return this._students.filter(student => student.id === studentId)[0];
     }
 
     updateStudent(studentId: number, name: string, surname: string) {
@@ -22,7 +22,7 @@ export class StudentService {
         student.setSurname(surname);
     }
     deleteStudent(studentId: number) {
-        this.students.splice(studentId);
+        this._students.splice(studentId);
     }
 
 }
